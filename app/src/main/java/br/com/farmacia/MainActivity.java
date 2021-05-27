4d45544692941c5b228fa2201b710a7fe3a11719
JonathanSilvaViana <jonathansilva@daum.net>
2021-05-27 16:53:19 -0300

    Ofertas

--- app/src/main/java/br/com/farmacia/MainActivity.java
+++ app/src/main/java/br/com/farmacia/MainActivity.java
@@ -2,13 +2,30 @@ package br.com.farmacia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 2665;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+                                      @Override
+                                      public void run() {
+                                          Intent saidasplashscreen = new Intent(MainActivity.this,
+                                                  OfertaActivity.class );
+                                          startActivity(saidasplashscreen);
+                                          finish();
+                                      }
+                                  },
+                //encerra o tempo de exibição
+                SPLASH_TIME_OUT);
+
     }
 }
---
+++ app/src/main/java/br/com/farmacia/OfertaActivity.java
@@ -0,0 +1,18 @@
+package br.com.farmacia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+
+public class OfertaActivity extends AppCompatActivity {
+
+    ActionBar barra;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oferta);
+
+    }
+}
